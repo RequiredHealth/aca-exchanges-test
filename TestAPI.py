8eb77876a282b369b4b4f7e26441a9aa0c39f859
eggtree <eggtree@requiredhealth.org>
2013-09-28 08:33:44 -0500

    Use a more standard requirements.txt file to capture dependencies. Add more tests

--- TestAPI.py
+++ TestAPI.py
@@ -1,10 +1,46 @@
 import requests
 
-class TestAPI:
-    def test_c(self):
-        assert 'c' == 'c'
+class TestPremium:
+    host = 'http://localhost:5000'
+
+    def test_premium_all_params(self):
         payload = {'lat': '39.68', 'long': '-122.48', 'age': '25', 'limit':'3'}
-        r = requests.get('http://localhost:5000/premium', params=payload)
+        r = requests.get(self.host + '/premium', params=payload)
         # check r.status_code
 	result = r.json()
 	assert result[0][0] == 'KP_CATA_015'
+
+    def test_premium_no_age(self):
+        payload = {'lat': '39.68', 'long': '-122.48', 'limit':'3'}
+        r = requests.get(self.host + '/premium', params=payload)
+        # check r.status_code
+        result = r.json()
+        assert result['message'] == 'Age is required'
+
+    def test_premium_no_limit(self):
+        payload = {'lat': '39.68', 'long': '-122.48', 'age': '25'}
+        r = requests.get(self.host + '/premium', params=payload)
+        # check r.status_code
+        result = r.json()
+        # TODO uncomment when working	
+	#assert len(result) == 4, 'There should be 4 results in the list'
+
+class TestState:
+    host = 'http://localhost:5000'
+
+    def test_state_county_CA_glenn(self):
+        payload = {'lat': '39.68', 'long': '-122.48'}
+        r = requests.get(self.host + '/state', params=payload)
+        # check r.status_code
+        result = r.json()
+        assert result['state'] == 'CA'
+	assert result['county'] == 'Glenn', 'county returned is %r' % result['county']
+
+    def test_state_county_CA_colusa(self):
+        payload = {'lat': '39', 'long': '-122'}
+        r = requests.get(self.host + '/state', params=payload)
+        # check r.status_code
+        result = r.json()
+        assert result['state'] == 'CA'
+        assert result['county'] == 'Colusa'
+
