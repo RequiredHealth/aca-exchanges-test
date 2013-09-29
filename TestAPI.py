7e68448095ee46b504d70c09d72ee0dc25aad75f
eggtree <eggtree@requiredhealth.org>
2013-09-29 14:57:53 -0500

    store host url to test in a module (non-public) attribute

--- TestAPI.py
+++ TestAPI.py
@@ -1,40 +1,40 @@
 import requests
 import nose
 
-class TestPremium:
-    host = 'http://localhost:5000'
+#_HOST_UNDER_TEST = 'http://localhost:5000'
+_HOST_UNDER_TEST = 'http://api.requiredhealth.org'
 
+class TestPremium:
     def test_all_params(self):
         payload = {'lat': '39.68', 'long': '-122.48', 'age': '25', 'limit':'3'}
-        r = requests.get(self.host + '/premium', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
         # check r.status_code
 	result = r.json()
 	assert result[0][0] == 'KP_CATA_015'
         nose.tools.assert_almost_equal(result[0][1], 202.17, places=2)
         assert len(result) == 3, '%r returned' % result
 
-
     def test__no_age(self):
         payload = {'lat': '39.68', 'long': '-122.48', 'limit':'3'}
-        r = requests.get(self.host + '/premium', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
         # check r.status_code
         result = r.json()
         assert result['message'] == 'Age is required'
 
     def test_no_limit(self):
         payload = {'lat': '39.68', 'long': '-122.48', 'age': '25'}
-        r = requests.get(self.host + '/premium', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
         # check r.status_code
         result = r.json()
 	assert len(result) == 4, '%r returned' % result
 
-class TestState:
-    host = 'http://localhost:5000'
 
+
+class TestState:
     def test_Artois_CA(self):
         #http://tools.wmflabs.org/geohack/geohack.php?pagename=Artois%2C_California&params=39_37_11_N_122_11_38_W_region:US_type:city
 	payload = {'lat': '39.619722', 'long':'-122.193889'}
-        r = requests.get(self.host + '/state', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/state', params=payload)
         # check r.status_code
         result = r.json()
         assert result['state'] == 'CA'
@@ -42,7 +42,7 @@ class TestState:
 
     def test_lat39_long122(self):
         payload = {'lat': '39', 'long': '-122'}
-        r = requests.get(self.host + '/state', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/state', params=payload)
         # check r.status_code
         result = r.json()
         assert result['state'] == 'CA'
@@ -50,7 +50,7 @@ class TestState:
 
     def test_TXStateCapitol(self):
         payload = {'lat': '30.274635', 'long': '-97.74039'}
-        r = requests.get(self.host + '/state', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/state', params=payload)
         # check r.status_code
         result = r.json()
         # returning CA/Butte for all locations outside of california right now
@@ -60,37 +60,34 @@ class TestState:
 
 
 class TestRegion:
-    host = 'http://localhost:5000'
-
     def test_CA_butte(self):
         payload = {'state': 'CA', 'county': 'BUTTE'}
-        r = requests.get(self.host + '/region', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/region', params=payload)
         # check r.status_code
         result = r.json()
         assert result == 1, 'region returned is %r' % result
 
     def test_CA_napa(self):
         payload = {'state': 'ca', 'county': 'napa'}
-        r = requests.get(self.host + '/region', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/region', params=payload)
         # check r.status_code
         result = r.json()
         assert result == 2, 'region returned is %r' % result
 
     def test_TX_travis(self):
         payload = {'state': 'TX', 'county': 'Travis'}
-        r = requests.get(self.host + '/region', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/region', params=payload)
         # check r.status_code
         result = r.json()
         assert result['message'] == "County Travis doesn't have a matching health region", \
             'region returned is %r' % result
 
 
-class TestPlan:
-    host = 'http://localhost:5000'
 
+class TestPlan:
     def test_region1_age25_limit2(self):
         payload = {'age': 25, 'region': 1, 'limit': 2}
-        r = requests.get(self.host + '/plan', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/plan', params=payload)
         # check r.status_code
         result = r.json()
         assert result[0][0] == 'KP_CATA_015'
@@ -99,7 +96,7 @@ class TestPlan:
 
     def test_region1_age40(self):
         payload = {'age': 40, 'region': 1}
-        r = requests.get(self.host + '/plan', params=payload)
+        r = requests.get(_HOST_UNDER_TEST + '/plan', params=payload)
         # check r.status_code
         result = r.json()
         assert result[1][0] == 'KP_BRNZ_004'
