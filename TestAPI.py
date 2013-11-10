71d0236f778d94e6916194e264652536395263b5
eggtree <eggtree@requiredhealth.org>
2013-11-10 13:12:39 -0600

    add test for new zip code and rating area based premium apis

--- TestAPI.py
+++ TestAPI.py
@@ -42,6 +42,89 @@ class TestPremium:
         nose.tools.assert_almost_equal(result[1]['Premium'], Decimal(258.58), places=2)
         assert len(result) == 6, 'Got %r results' % len(result)
 
+    # osceola county
+    def test_Kissimmee_FL_age50(self):
+        # Osceola county
+        payload = {'lat': '28.283333', 'long': '-81.413222', 'age':50}
+        r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        assert len(result) == 101, 'Got {} results'.format(len(result))
+        nose.tools.assert_almost_equal(result[0]['Premium'], Decimal(239.59), places=1)        
+        nose.tools.assert_almost_equal(result[100]['Premium'], Decimal(635.23), places=1)
+        print result
+
+    # osceola county
+    def test_Kissimmee_FL_age50_zpremium(self):
+        payload = {'zip':34741, 'age':50}
+        r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        assert len(result) == 101, 'Got {} results'.format(len(result))
+        nose.tools.assert_almost_equal(result[0]['Premium'], Decimal(239.59), places=1)
+        nose.tools.assert_almost_equal(result[100]['Premium'], Decimal(635.23), places=1)
+
+    # hillsborough county
+    def test_Tampa_FL_age50(self):
+        payload = {'zip':33601, 'age':50}
+        r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        assert len(result) == 106, 'Got {} results'.format(len(result))
+        # todo have a better way to indicate error tolerance than just place of decimals
+        nose.tools.assert_almost_equal(result[0]['Premium'], Decimal(219.60), places=1)
+        nose.tools.assert_almost_equal(result[105]['Premium'], Decimal(597.61), places=1)
+
+    # hillsborough county
+    def test_rating_area_28_FL_age50(self):
+        payload = {'state': 'FL', 'rating_area' : 28, 'age' : 50}
+        r = requests.get(_HOST_UNDER_TEST + '/ra_premium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        assert len(result) == 106, 'Got {} results'.format(len(result))
+        # todo have a better way to indicate error tolerance than just place of decimals
+        nose.tools.assert_almost_equal(result[0]['Premium'], Decimal(219.60), places=1)
+        nose.tools.assert_almost_equal(result[105]['Premium'], Decimal(597.61), places=1)
+  
+    # miami-dade county
+    def test_coconut_grove_FL(self):
+        payload = {'zip':33133, 'age':50}
+        r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        assert len(result) == 141, 'Got {} results'.format(len(result))
+        # todo have a better way to indicate error tolerance than just place of decimals
+        nose.tools.assert_almost_equal(result[0]['Premium'], Decimal(185.72), places=1)
+        nose.tools.assert_almost_equal(result[140]['Premium'], Decimal(716.03), places=1)
+ 
+    # miami-dade county
+    def test_rating_area_43_FL_age50(self):
+        payload = {'state' : 'FL', 'rating_area': 43, 'age':50}
+        r = requests.get(_HOST_UNDER_TEST + '/ra_premium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        print r.content
+        assert len(result) == 141, 'Got {} results'.format(len(result))
+        # todo have a better way to indicate error tolerance than just place of decimals
+        nose.tools.assert_almost_equal(result[0]['Premium'], Decimal(185.72), places=1)
+        nose.tools.assert_almost_equal(result[140]['Premium'], Decimal(716.03), places=1)
+
+
+
+    def test_non_existent_zip(self):
+        payload = {'zip':99999, 'age':50}
+        r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
+        # check r.status_code
+        assert r.status_code == 404
+
+# with more insurers now, there should be no zips without coverage    
+#    def test_valid_zip_with_no_coverage(self):
+#        payload = {'zip':32601, 'age':50}
+#        r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
+#        # check r.status_code
+#        assert r.status_code == 404, r.content
+
+
     def test_all_params(self):
         payload = {'lat': '39.68', 'long': '-122.48', 'age': '25', 'limit':'3'}
         r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
