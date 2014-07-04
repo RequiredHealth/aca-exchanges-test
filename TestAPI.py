29ef6e0310189e39b5d6b56b1e7efd3a8b74a3f9
root <root@a11f9d65c84e.(none)>
2014-07-04 07:36:11 +0000

    switch to new show_catastrophic flag
    -remove all references to limit_catastrophic flag, that behaviour
    is now the default
    -add in show_catastrophic to some tests so that we dont have to
    calculate which is the first plan in the returned list. Still
    plenty of tests not using show_catastrophic

--- TestAPI.py
+++ TestAPI.py
@@ -27,24 +27,38 @@ class TestPremium:
     # multithreaded server
     _multiprocess_shared_ = True
 
+    def test_hidalgo_county_age_54(self):
+        payload = {'zip':78501, 'age':54}
+        r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        assert len(result) == 24, 'Got {} results'.format(len(result))
+
     def test_orange_county_age_46(self):
         payload = {'lat': '33.74', 'long': '-117.88', 'age': 46}
         r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
-        assert len(result) == 6, 'Got %r results' % len(result)
+        assert len(result) == 2, 'Got %r results' % len(result)
         #print result
+
+    def test_orange_county_age_26(self):
+        payload = {'lat': '33.74', 'long': '-117.88', 'age': 26}
+        r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
+        # check r.status_code
+        result = json.loads(r.content, parse_float=Decimal)
+        assert len(result) == 6, 'Got %r results' % len(result)
 
     def test_la_county_age_32(self):
         payload = {'lat': '34.07', 'long': '-118.40', 'age': 32}
         r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
-        assert len(result) == 6, 'Got %r results' % len(result)
+        assert len(result) == 2, 'Got %r results' % len(result)
         #print result
 
     def test_region1_age40(self):
-        payload = {'lat': '39.68', 'long': '-122.48', 'age': 40}
+        payload = {'lat': '39.68', 'long': '-122.48', 'age': 40, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -58,7 +72,8 @@ class TestPremium:
     # osceola county
     def test_Kissimmee_FL_age50(self):
         # Osceola county
-        payload = {'lat': '28.283333', 'long': '-81.413222', 'age':50}
+        payload = {'lat': '28.283333', 'long': '-81.413222', 'age':50,
+            'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/premium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -69,7 +84,7 @@ class TestPremium:
 
     # osceola county
     def test_Kissimmee_FL_age50_zpremium(self):
-        payload = {'zip':34741, 'age':50}
+        payload = {'zip':34741, 'age':50, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -79,7 +94,7 @@ class TestPremium:
 
     # hillsborough county
     def test_Tampa_FL_age50(self):
-        payload = {'zip':33601, 'age':50}
+        payload = {'zip':33601, 'age':50, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -90,7 +105,7 @@ class TestPremium:
 
     # travis county
     def test_Austin_TX_age50(self):
-        payload = {'zip':78731, 'age':50}
+        payload = {'zip':78731, 'age':50, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -101,7 +116,7 @@ class TestPremium:
     
 
     def test_Austin25(self):
-        payload = {'zip':78731, 'age':25, 'limit_catastrophic': True}
+        payload = {'zip':78731, 'age':25}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         assert result[0]['Tier'] == 'Catastrophic'
@@ -110,7 +125,7 @@ class TestPremium:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
     def test_Austin30(self):
-        payload = {'zip':78731, 'age':30, 'limit_catastrophic': True}
+        payload = {'zip':78731, 'age':30}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         assert result[0]['Tier'] == 'Bronze', 'Got {} expected Bronze'.format(result[0]['Tier'])
@@ -121,8 +136,8 @@ class TestPremium:
                 assert res['Deductible'] == 6000
 
     def test_Austin25_5xFPL(self):
-        payload = {'zip':78731, 'age':25, 'limit_catastrophic': True, 
-            'household_income': 5 * FPL, 'household_size': 1}
+        payload = {'zip':78731, 'age':25, 'household_income': 5 * FPL,
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         assert result[0]['Tier'] == 'Catastrophic'
@@ -131,8 +146,8 @@ class TestPremium:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
     def test_Austin30_5xFPL(self):
-        payload = {'zip':78731, 'age':30, 'limit_catastrophic': True,
-            'household_income': 5 * FPL, 'household_size': 1}
+        payload = {'zip':78731, 'age':30, 'household_income': 5 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         assert result[0]['Tier'] == 'Bronze', 'Got {} expected Bronze'.format(result[0]['Tier'])
@@ -144,8 +159,8 @@ class TestPremium:
 
 
     def test_Austin25_1xFPL(self):
-        payload = {'zip':78731, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78731, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         # in theory this could be silver because the are likely to be both silver
@@ -163,48 +178,48 @@ class TestPremium:
 
     def test_CentralTexas25_1xFPL(self):
         # Travis county
-        payload = {'zip':78731, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78731, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Bastrop county
-        payload = {'zip':78602, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78602, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Blanco county
-        payload = {'zip':78070, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78070, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Burnet county
-        payload = {'zip':73301, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':73301, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Caldwell county
-        payload = {'zip':78610, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78610, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Comal county
-        payload = {'zip':78602, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78602, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
@@ -212,48 +227,48 @@ class TestPremium:
 
 
         # Williamson county
-        payload = {'zip':78646, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78646, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Fayette county
-        payload = {'zip':78932, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78932, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Gillespie county
-        payload = {'zip':76856, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':76856, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Hays county
-        payload = {'zip':78620, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78620, 'age':25, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Lee county
-        payload = {'zip':76578, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':76578, 'age':25, 'household_income': 1 * FPL,
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
             assert res.get('Deductible') >= 0, 'Missing deductible: {}'.format(res)
 
         # Llano county
-        payload = {'zip':76831, 'age':25, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':76831, 'age':25, 'household_income': 1 * FPL,
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         for res in result:
@@ -261,8 +276,8 @@ class TestPremium:
 
 
     def test_Austin30_1xFPL(self):
-        payload = {'zip':78731, 'age':30, 'limit_catastrophic': True,
-            'household_income': 1 * FPL, 'household_size': 1}
+        payload = {'zip':78731, 'age':30, 'household_income': 1 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         # in theory this could be silver because the are likely to be both silver
@@ -279,8 +294,8 @@ class TestPremium:
                assert res['Deductible'] == 500
 
     def test_Austin30_2xFPL(self):
-        payload = {'zip':78731, 'age':30, 'limit_catastrophic': True,
-            'household_income': 2 * FPL, 'household_size': 1}
+        payload = {'zip':78731, 'age':30, 'household_income': 2 * FPL, 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         # in theory this could be silver because the are likely to be both silver
@@ -292,8 +307,8 @@ class TestPremium:
 
 
     def test_Austin30_2pt4xFPL(self):
-        payload = {'zip':78731, 'age':30, 'limit_catastrophic': True,
-            'household_income': int(2.4 * FPL), 'household_size': 1}
+        payload = {'zip':78731, 'age':30, 'household_income': int(2.4 * FPL), 
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         # in theory this could be silver because the are likely to be both silver
@@ -304,8 +319,8 @@ class TestPremium:
                assert res['Deductible'] == 5000
 
     def test_Austin30_pt4xFPL(self):
-        payload = {'zip':78731, 'age':30, 'limit_catastrophic': True,
-            'household_income': int(0.4 * FPL), 'household_size': 1}
+        payload = {'zip':78731, 'age':30, 'household_income': int(0.4 * FPL),
+            'household_size': 1}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         result = json.loads(r.content, parse_float=Decimal)
         # in theory this could be silver because the are likely to be both silver
@@ -325,7 +340,7 @@ class TestPremium:
     # Gundersen Health Plan, Inc does NOT offer plans in Linn county (rating area 6)
     def test_Linn_county__IA_age50(self):
         # Cedar rapids is in Linn county
-        payload = {'zip':52401, 'age':50}
+        payload = {'zip':52401, 'age':50, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -337,7 +352,7 @@ class TestPremium:
     # Gundersen Health Plan, Inc does offer plans in Clayton county (rating area 6)
     def test_Clayton_county_IA_age50(self):
         # Elkader is in Clayton county
-        payload = {'zip':52043, 'age':50}
+        payload = {'zip':52043, 'age':50, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -350,7 +365,7 @@ class TestPremium:
     # hillsborough county
     def test_Tampa_FL_age50(self):
         # tampa
-        payload = {'zip' : 33660, 'age' : 50}
+        payload = {'zip' : 33660, 'age' : 50, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
@@ -375,7 +390,7 @@ class TestPremium:
     # miami-dade county
     def test_33101_age50(self):
         # miami
-        payload = {'zip' : 33101, 'age':50}
+        payload = {'zip' : 33101, 'age':50, 'show_catastrophic': True}
         r = requests.get(_HOST_UNDER_TEST + '/zpremium', params=payload)
         # check r.status_code
         result = json.loads(r.content, parse_float=Decimal)
